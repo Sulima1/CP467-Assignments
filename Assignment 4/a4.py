a1e805aa4a3eab79c9bb684d38ca4856a103c6b9
Matthew Borkowski <55469998+Sulima1@users.noreply.github.com>
2023-11-15 01:16:22 -0500

    A4

--- Assignment 4/a4.py
+++ Assignment 4/a4.py
@@ -28,7 +28,7 @@ def faceRecognition(faceInput, faceGrey):
             cv.putText(roiColor, 'Eye', (ex, ey), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
 
-    cv.imwrite("Assignment 4/outputs/faceOuput.png", faceInput)
+    cv.imwrite("Assignment 4/outputs/faceOutput.png", faceInput)
 
     return
 
@@ -68,28 +68,25 @@ def keyPointDetection(faceInput, carInput, faceGrey, carGrey):
 def imageManipulation(backgroundImage, figureImage):
 
     figureImage = cv.resize(figureImage, (figureImage.shape[0]//2, figureImage.shape[1]//2))
-
-    # Assuming the source and destination images have the same size
     mask = 255 * np.ones(figureImage.shape, figureImage.dtype)
 
-    # This is where the CENTER of the source image will be placed
+    #this is where the CENTER of the source image will be placed
     center = (backgroundImage.shape[1] // 2, backgroundImage.shape[0] // 2 - 250)
 
-    # Convert BGR to HSV
+    #convert BGR to HSV and reduce saturation
     hsv = cv.cvtColor(figureImage, cv.COLOR_BGR2HSV)
 
-    # Reduce saturation
     saturationFactor = 0.4
     hsv[:, :, 1] = hsv[:, :, 1] * saturationFactor
 
-    # Convert back to BGR
+    #convert back to BGR
     desaturatedImage = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
 
-    # Poisson blending
+    #blend images
     result = cv.seamlessClone(desaturatedImage, backgroundImage, mask, center, cv.MIXED_CLONE)
 
-    # Display the result
-    cv.imwrite("Assignment 4/outputs/mixedImage.jpg", result) 
+    #display the result
+    cv.imwrite("Assignment 4/outputs/mixedImage.png", result) 
     
 
     return
@@ -97,34 +94,34 @@ def imageManipulation(backgroundImage, figureImage):
 def imageStitching(leftImage, leftImageGrey, rightImage, rightImageGrey):
 
     sift = cv.SIFT_create()
-    # Find keypoints and descriptors
+    #find keypoints and descriptors
     keypoint1, descriptor1 = sift.detectAndCompute(leftImageGrey, None)
     keypoint2, descriptor2 = sift.detectAndCompute(rightImageGrey, None)
 
-    # Use a FLANN based matcher to find matches
+    #use a FLANN based matcher to find matches
     matcher = cv.FlannBasedMatcher(dict(algorithm=0, trees=5), dict(checks=50))
     matches = matcher.knnMatch(descriptor1, descriptor2, k=2)
 
-    # Ratio test to get good matches
+    #ratio test to get good matches
     goodMatches = []
     for m, n in matches:
         if m.distance < 0.7 * n.distance:
             goodMatches.append(m)
 
-    # Extract matched keypoints
+    #extract matched keypoints
     sourcePoints = np.float32([keypoint1[m.queryIdx].pt for m in goodMatches]).reshape(-1, 1, 2)
     destinationPoints = np.float32([keypoint2[m.trainIdx].pt for m in goodMatches]).reshape(-1, 1, 2)
 
-    # Find the homography matrix
+    #find the homography matrix
     H, _ = cv.findHomography(sourcePoints, destinationPoints, cv.RANSAC, 5.0)
 
-    # Warp leftImage to align with rightImage
+    #warp leftImage to align with rightImage
     result = cv.warpPerspective(leftImage, H, (rightImage.shape[1] + leftImage.shape[1], rightImage.shape[0]))
 
-    # Copy leftImage to the result image
+    #copy leftImage to the result image
     result[0:rightImage.shape[0], 0:rightImage.shape[1]] = rightImage
 
-    cv.imwrite("Assignment 4/outputs/Panorama.jpg", result)
+    cv.imwrite("Assignment 4/outputs/Panorama.png", result)
 
     return
 
